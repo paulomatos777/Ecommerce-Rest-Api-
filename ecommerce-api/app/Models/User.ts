ac64bc8892f1af53f019a98ad7e0975742d52c4c
Paulo Matos <paulomatos4554@gmail.com>
2022-05-28 15:43:25 -0300

    config uuid for user id

--- ecommerce-api/app/Models/User.ts
+++ ecommerce-api/app/Models/User.ts
@@ -1,11 +1,15 @@
+import { v4 as uuidv4 } from 'uuid'
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, beforeCreate } from '@ioc:Adonis/Lucid/Orm'
 
 export default class User extends BaseModel {
   public static table = 'users'
 
   @column({ isPrimary: true })
   public id: number
+
+  @column({})
+  public secure_id: uuidv4
 
   @column()
   public email: string
@@ -18,4 +22,9 @@ export default class User extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @beforeCreate()
+  public static assignUuid(user: User) {
+    user.secure_id = uuidv4()
+  }
 }
