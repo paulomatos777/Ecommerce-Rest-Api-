b9ef5a19a4a4f6de019cba63f85d25dcee9e6b97
Paulo Matos <paulomatos4554@gmail.com>
2022-05-29 08:52:42 -0300

    (feat): api token email identify

--- ecommerce-api/app/Controllers/Http/AuthController.ts
+++ ecommerce-api/app/Controllers/Http/AuthController.ts
@@ -1,12 +1,16 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class AuthController {
   public async login({ auth, request, response }: HttpContextContract) {
     const email = request.input('email')
     const password = request.input('password')
     try {
-      const token = await auth.use('api').attempt(email, password, { expiresIn: '30mins' })
-      return token
+      const user = await User.findBy('email', email)
+      const token = await auth
+        .use('api')
+        .attempt(email, password, { expiresIn: '30mins', name: user?.serialize().email })
+      return { token, user: user?.serialize() }
     } catch {
       return response.badRequest('Invalid credentials')
     }
