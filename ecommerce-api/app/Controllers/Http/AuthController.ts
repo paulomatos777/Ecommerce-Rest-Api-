9d77af83b4784010e23030f61969c860cb2a295c
Paulo Matos <paulomatos4554@gmail.com>
2022-05-28 22:03:03 -0300

    (feat):Auth Controller

---
+++ ecommerce-api/app/Controllers/Http/AuthController.ts
@@ -0,0 +1,14 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+  public async login({ auth, request, response }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+    try {
+      const token = await auth.use('api').attempt(email, password)
+      return token
+    } catch {
+      return response.badRequest('Invalid credentials')
+    }
+  }
+}
--- ecommerce-api/start/routes.ts
+++ ecommerce-api/start/routes.ts
@@ -24,13 +24,4 @@ Route.get('/', async () => {
   return { hello: 'world' }
 })
 //generating api token
-Route.post('login', async ({ auth, request, response }) => {
-  const email = request.input('email')
-  const password = request.input('password')
-  try {
-    const token = await auth.use('api').attempt(email, password)
-    return token
-  } catch {
-    return response.badRequest('Invalid credentials')
-  }
-})
+Route.post('/login', 'AuthController.login')
